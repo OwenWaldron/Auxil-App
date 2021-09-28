b1971061bd5d14bc3a23d99684893eb3b7a948bd
Owen Waldron <owaldron@tfs.ca>
2021-09-28 15:42:00 -0400

    Added view paths

---
+++ Auxil/Views/DataView.swift
@@ -0,0 +1,21 @@
+//
+//  DataView.swift
+//  Auxil
+//
+//  Created by Owen Waldron on 2021-09-28.
+//
+
+import SwiftUI
+
+struct DataView: View {
+    var body: some View {
+        // TODO DataView
+        Text("Hello, DataView!")
+    }
+}
+
+struct DataView_Previews: PreviewProvider {
+    static var previews: some View {
+        DataView()
+    }
+}
